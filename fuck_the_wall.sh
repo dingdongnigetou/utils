4880cdd5d8778981ad5d1f2062064ad84997a893
Finch <dingdongnigetou@gmail.com>
2018-08-12 12:09:01 +0800

    Update fuck_the_wall.sh

--- fuck_the_wall.sh
+++ fuck_the_wall.sh
@@ -9,13 +9,13 @@ pip install shadowsocks
 echo "### config ###"
 echo "
 {
-	"server":"0.0.0.0",
-	"server_port":443,
-	"local_address": "127.0.0.1",
-	"local_port":1080,
-	"password":"put your password here"
-	"timeout":300,
-	"method":"aes-256-cfb"
+	\"server\":\"0.0.0.0\",
+	\"server_port\":443,
+	\"local_address\": \"127.0.0.1\",
+	\"local_port\":1080,
+	\"password\":\"put your password here\",
+	\"timeout\":300,
+	\"method\":\"aes-256-cfb\"
 }
 " >/etc/shadowsocks.json
 
